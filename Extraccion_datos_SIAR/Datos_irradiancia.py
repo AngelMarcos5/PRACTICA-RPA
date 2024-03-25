ee89027c35427816fac1e2e583288dc65c7a4030
AngelMarcos5 <alu0101214559@ull.edu.es>
2024-03-25 21:20:01 +0000

    Primera versión de la extracción de datos de Irradiancia

---
+++ Extraccion_datos_SIAR/Datos_irradiancia.py
@@ -0,0 +1,37 @@
+import requests
+from datetime import datetime, timedelta
+import csv
+
+# Configuración inicial
+clave_api = "_4MMKz3eDiSwUtRqqVi62W-a8XDVmoEgqa0o_bGC_o9vjOvROu"
+id_estacion = "TF164"
+fecha_final = datetime.today()
+fecha_inicial = fecha_final - timedelta(days=2)
+
+# Formatear las fechas para la solicitud
+fecha_inicial_str = fecha_inicial.strftime('%Y-%m-%d')
+fecha_final_str = fecha_final.strftime('%Y-%m-%d')
+
+# Construir la URL de la solicitud con la clave API y el ID de la estación
+url = f"https://servicio.mapama.gob.es/apisiar/API/v1/Datos/Diarios/Estacion?Id=TF164&FechaInicial={fecha_inicial_str}&FechaFinal={fecha_final_str}&ClaveAPI={clave_api}"
+
+# Realizar la solicitud
+respuesta = requests.get(url)
+
+# Verificar y procesar la respuesta
+if respuesta.status_code == 200:
+    datos = respuesta.json()
+    nombre_archivo_csv = "datos_irradiancia.csv"
+
+    with open(nombre_archivo_csv, 'w', newline='') as archivo_csv:
+        escritor = csv.writer(archivo_csv)
+        escritor.writerow(['Fecha', 'Radiacion'])
+
+        # Asumiendo que 'Radiacion' es uno de los campos en la respuesta; ajustar según sea necesario
+        for registro in datos['Datos']:
+            escritor.writerow([registro['Fecha'], registro.get('Radiacion', 'No Disponible')])
+
+    print(f"Datos guardados en {nombre_archivo_csv}")
+else:
+    print(f"Error al realizar la solicitud: {respuesta.status_code}, Detalles: {respuesta.text}")
+
